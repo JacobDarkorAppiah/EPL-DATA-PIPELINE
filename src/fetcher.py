320f36f77ea5083db2c6c77a75776c8a81718a94
Jacob Darkor Appiah <jdarkoappiah@gmail.com>
2026-02-27 23:21:39 +0000

    edited a line in fetcher.py

--- src/fetcher.py
+++ src/fetcher.py
@@ -22,7 +22,7 @@ def fetch_latest_stats():
         logger.info("Attempting to fetch latest stats from FBref...")
         
         # Human-like delay: Don't rush the server!
-        time.sleep(3) 
+        time.sleep(1) # Faster, but slightly riskier for bot detectio
         
         response = requests.get(url, headers=headers, timeout=15)
         
